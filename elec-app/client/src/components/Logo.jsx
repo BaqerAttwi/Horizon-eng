2c89a1142610eca16a3dcd4cbbbe64a9fefec056
BaqerAttwi <mhmd.baqer.attwi@gmail.com>
2026-06-28 00:27:38 +0300

    fix: remove white logo wrapper (PNG has baked-in bg), add zoomed SVG favicon

--- elec-app/client/src/components/Logo.jsx
+++ elec-app/client/src/components/Logo.jsx
@@ -1,7 +1,5 @@
-export default function Logo({ size = 160, style, className = '' }) {
+export default function Logo({ size = 160, style }) {
   return (
-    <span className={`logo-wrapper ${className}`} style={{ display: 'inline-flex', background: '#fff', borderRadius: 4, padding: 3, lineHeight: 0, ...style }}>
-      <img src="/LogoHorizonLB.png" alt="Horizon LB" style={{ width: size, maxWidth: '100%', height: 'auto', display: 'block', borderRadius: 2 }} />
-    </span>
+    <img src="/LogoHorizonLB.png" alt="Horizon LB" style={{ width: size, maxWidth: '100%', height: 'auto', display: 'block', borderRadius: 4, ...style }} />
   );
 }
